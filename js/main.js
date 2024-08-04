05b7e1f8bc2ae15a59465b6266cd4fe419debc24
sandrarolgom <sandraroldan210900@gmail.com>
2024-08-04 12:40:47 +0200

    gatos en java

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const kittenOne = `<li class="card">
   <img
     class="card_img"
     src="https://dev.adalab.es/gato-siames.webp"
-    alt="gatito"
+    alt="siames-cat"
   />
   <h3 class="card_title">Anastacio</h3>
   <h4 class="card_race">Siamés</h4>
@@ -19,4 +19,32 @@ const kittenOne = `<li class="card">
    </p>
 </article>
 </li>`;
-list.innerHTML=kittenOne;
+const kittenTwo = `<li class="card">
+            <img
+              class="card_img"
+              src="https://dev.adalab.es/sphynx-gato.webp"
+              alt="sphynx-cat"
+            />
+            <h3 class="card_title">Fiona</h3>
+            <h4 class="card_race">Sphynx</h4>
+            <p class="card_description">
+              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+              hasta con pinta de alienígena han llegado a definir a esta raza
+              gatuna que se caracteriza por la «ausencia» de pelo.
+            </p>
+          </li>`;
+const kittenThree= `<li class="card">
+            <img
+              class="card_img"
+              src="https://dev.adalab.es/maine-coon-cat.webp"
+              alt="maine-coon-cat"
+            />
+            <h3 class="card_title">Cielo</h3>
+            <h4 class="card_race">Maine Coon</h4>
+            <p class="card_description">
+              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+              bella mirada se ha convertido en una de sus señas de identidad.
+              Sus ojos son grandes y las orejas resultan largas y en punta.
+            </p>
+          </li>`;
+list.innerHTML= kittenOne + kittenTwo + kittenThree;
